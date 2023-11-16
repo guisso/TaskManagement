55efead681a214d4e141892aec8c302c74622ccf
Guisso <luis.guisso@ifnmg.edu.br>
2023-11-16 13:43:18 -0300

    Special task added

---
+++ src/main/java/io/github/guisso/taskmanagement/task/SpecialTask.java
@@ -0,0 +1,49 @@
+/*
+ * CC BY-NC-SA 4.0
+ *
+ * Copyright 2022 Luis Guisso &lt;luis dot guisso at ifnmg dot edu dot br&gt;.
+ *
+ * Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+ *
+ * You are free to:
+ *   Share - copy and redistribute the material in any medium or format
+ *   Adapt - remix, transform, and build upon the material
+ *
+ * Under the following terms:
+ *   Attribution - You must give appropriate credit, provide 
+ *   a link to the license, and indicate if changes were made.
+ *   You may do so in any reasonable manner, but not in any 
+ *   way that suggests the licensor endorses you or your use.
+ *   NonCommercial - You may not use the material for commercial purposes.
+ *   ShareAlike - If you remix, transform, or build upon the 
+ *   material, you must distribute your contributions under 
+ *   the same license as the original.
+ *   No additional restrictions - You may not apply legal 
+ *   terms or technological measures that legally restrict 
+ *   others from doing anything the license permits.
+ *
+ * Notices:
+ *   You do not have to comply with the license for elements 
+ *   of the material in the public domain or where your use 
+ *   is permitted by an applicable exception or limitation.
+ *   No warranties are given. The license may not give you 
+ *   all of the permissions necessary for your intended use. 
+ *   For example, other rights such as publicity, privacy, 
+ *   or moral rights may limit how you use the material.
+ */
+package io.github.guisso.taskmanagement.task;
+
+/**
+ * Special task
+ * 
+ * CREATE TABLE `tarefaespecial` ( `id` bigint(20) unsigned NOT NULL, `especial`
+ * tinyint(1) DEFAULT 0, PRIMARY KEY (`id`), CONSTRAINT `tarefaespecial_ibfk_1`
+ * FOREIGN KEY (`id`) REFERENCES `tarefa` (`id`) ) ENGINE=InnoDB DEFAULT
+ * CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+ *
+ * @author Luis Guisso &lt;luis.guisso at ifnmg.edu.br&gt;
+ */
+public class SpecialTask
+        extends Task {
+
+}
