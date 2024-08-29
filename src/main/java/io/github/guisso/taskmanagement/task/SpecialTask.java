c942e22a6255f748103fdda4bb2b29e7dfac06ef
Guisso <luis.guisso@ifnmg.edu.br>
2024-08-29 11:04:16 -0300

    Minor changes

--- src/main/java/io/github/guisso/taskmanagement/task/SpecialTask.java
+++ src/main/java/io/github/guisso/taskmanagement/task/SpecialTask.java
@@ -37,7 +37,7 @@ package io.github.guisso.taskmanagement.task;
  * Special task
  *
  * @author Luis Guisso &lt;luis.guisso at ifnmg.edu.br&gt;
- * @version 0.1, 2023-11-16
+ * @version 0.2, 2024-08-29
  */
 public class SpecialTask
         extends Task {
